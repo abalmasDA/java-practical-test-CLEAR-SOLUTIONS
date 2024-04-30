c21a6791352e3af1173e4c0dab729f06b53b3853
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-04-30 21:59:40 +0300

    User service refactoring

--- src/main/java/com/clearsolutions/javapracticaltest/service/impl/UserServiceImpl.java
+++ src/main/java/com/clearsolutions/javapracticaltest/service/impl/UserServiceImpl.java
@@ -5,19 +5,12 @@ import com.clearsolutions.javapracticaltest.exception.UserNotFoundException;
 import com.clearsolutions.javapracticaltest.mapper.DataMapper;
 import com.clearsolutions.javapracticaltest.model.User;
 import com.clearsolutions.javapracticaltest.service.UserService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.clearsolutions.javapracticaltest.service.ValidationService;
+import com.clearsolutions.javapracticaltest.util.PatchUtil;
 import com.github.fge.jsonpatch.JsonPatch;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Validator;
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,18 +20,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+  private final ValidationService validationService;
+
   private final DataMapper<UserDto, User> userMapper;
 
-  private final ObjectMapper objectMapper;
-
-  private final Validator validator;
-
-  @Value("${user.minValidAge}")
-  private int minValidAge;
-
-  private long nextId = 1L;
+  private final PatchUtil patchUtil;
 
   private final List<User> users;
+
+  private long nextId = 1L;
 
   /**
    * Creates a new user.
@@ -47,7 +37,7 @@ public class UserServiceImpl implements UserService {
    * @return the newly created user as a data transfer object
    */
   public UserDto create(UserDto userDto) {
-    validateUserAge(userDto.getBirthDate());
+    validationService.validateUserAge(userDto.getBirthDate());
     User user = userMapper.toEntity(userDto);
     user.setId(nextId++);
     users.add(user);
@@ -64,7 +54,8 @@ public class UserServiceImpl implements UserService {
   public UserDto updatePartially(long id, JsonPatch patchData) {
     User user = findById(id);
     UserDto userDto = userMapper.toDto(user);
-    UserDto userDtoPatched = applyPatchToDto(patchData, userDto, UserDto.class);
+    UserDto userDtoPatched = patchUtil.applyPatch(patchData, userDto, UserDto.class);
+    validationService.validateDto(userDtoPatched);
     userMapper.updateEntity(userDtoPatched, user);
     return userMapper.toDto(user);
   }
@@ -99,38 +90,12 @@ public class UserServiceImpl implements UserService {
    * @return a list of UserDto objects that fall within the specified birthdate range
    */
   public List<UserDto> searchUsersByBirthDateRange(LocalDate fromDate, LocalDate toDate) {
-    validateDateRange(fromDate, toDate);
+    validationService.validateUserDateRange(fromDate, toDate);
     return users.stream()
         .filter(
             user -> !user.getBirthDate().isBefore(fromDate) && !user.getBirthDate().isAfter(toDate))
         .map(userMapper::toDto)
         .toList();
-  }
-
-  /**
-   * Validates the user's age based on the minimum valid age.
-   *
-   * @param birthDate The birthdate of the user to validate.
-   * @throws IllegalArgumentException if the user's age is less than or equal to the minimum valid
-   */
-  private void validateUserAge(LocalDate birthDate) {
-    Period period = Period.between(birthDate, LocalDate.now());
-    if (period.getYears() <= minValidAge) {
-      throw new IllegalArgumentException("Age must be greater than " + minValidAge);
-    }
-  }
-
-  /**
-   * Validates if the fromDate is before the toDate.
-   *
-   * @param fromDate the starting date
-   * @param toDate   the ending date
-   * @throws IllegalArgumentException if fromDate is after toDate
-   */
-  private void validateDateRange(LocalDate fromDate, LocalDate toDate) {
-    if (fromDate.isAfter(toDate)) {
-      throw new IllegalArgumentException("From date must be before To date");
-    }
   }
 
   /**
@@ -145,28 +110,6 @@ public class UserServiceImpl implements UserService {
         .filter(user -> id.equals(user.getId()))
         .findFirst()
         .orElseThrow(() -> new UserNotFoundException(id));
-  }
-
-  /**
-   * Applies a JSON patch to a target DTO object and validates the patched object.
-   *
-   * @param patchData  The JSON patch data to apply
-   * @param targetBean The target DTO object to apply the patch to
-   * @param classBean  The class of the target DTO object
-   * @param <T>        The type of the target DTO object
-   * @return The patched and validated DTO object
-   * @throws ConstraintViolationException If the patched bean violates any constraints.
-   */
-  @SneakyThrows
-  private <T> T applyPatchToDto(JsonPatch patchData, T targetBean, Class<T> classBean) {
-    JsonNode target = objectMapper.convertValue(targetBean, JsonNode.class);
-    JsonNode patched = patchData.apply(target);
-    T beanPatched = objectMapper.convertValue(patched, classBean);
-    Set<ConstraintViolation<T>> violations = validator.validate(beanPatched);
-    if (!violations.isEmpty()) {
-      throw new ConstraintViolationException("Validation failed for patched part", violations);
-    }
-    return beanPatched;
   }
 
 }
