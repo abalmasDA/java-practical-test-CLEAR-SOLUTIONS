f13f7f8e1c9197286cb46304150e57965da079e3
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-05-01 16:21:43 +0300

    minor refactoring error response

--- src/main/java/com/clearsolutions/javapracticaltest/exception/ErrorResponse.java
+++ src/main/java/com/clearsolutions/javapracticaltest/exception/ErrorResponse.java
@@ -1,12 +1,8 @@
 package com.clearsolutions.javapracticaltest.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 @AllArgsConstructor
-@Getter
-@Setter
 public class ErrorResponse {
 
   private String message;
